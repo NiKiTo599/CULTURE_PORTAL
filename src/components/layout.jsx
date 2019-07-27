3d71a18326b5bfe999c70f930784b2d00e0cc659
Vola Graholska <vola.graholska@gmail.com>
2019-07-28 00:34:22 +0300

    feat: add basic pages layout

---
+++ src/components/layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Footer from './footer';
+import Header from './header';
+import '../styles/style.css'
+
+const Layout = (props) => {
+    return (
+        <div>
+            <Header />
+            {props.children}
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
