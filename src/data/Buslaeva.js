788a119ac86731a2961578217717d2692adc9fcf
Cat <jarolika30@gmail.com>
2019-07-28 16:58:46 +0300

    feat: prepare and add data about theater directors

---
+++ src/data/Buslaeva.js
@@ -0,0 +1,9 @@
+exports.frontmatter = {
+    id: 2,
+    path: '/Buslaeva',
+    name: 'Вероника Александровна Буслаева',
+    date: '3 октября 1978',
+    img: 'Łarysa_Aleksandr.jpg',
+    youtube: 'https://www.youtube.com/watch?v=icg14Eihosw',
+    place_of_birth: 'Минск',
+}
