f478ed6d3e66cba06e9697f9ae53a3a002512b9b
Cat <jarolika30@gmail.com>
2019-07-28 18:51:26 +0300

    feat: implement search input component

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,10 +13,18 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: `${__dirname}/src/data/images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'data',
+        path: `${__dirname}/src/data/`,
       },
     },
     'gatsby-transformer-sharp',
+    'gatsby-transformer-javascript-frontmatter',
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-manifest',
