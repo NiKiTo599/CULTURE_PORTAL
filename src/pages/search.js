36950bc4be4843beca11eac71b0e3f4ad43b79a3
NiKiTo599 <akulichnikita0@gmail.com>
2019-07-28 21:27:34 +0300

    feat: add layout to a search

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,39 +1,39 @@
-import React from "react";
-import './search.css';
+import React from "react"
+import Layout from "../components/layout"
+import "./search.css"
 
 class Search extends React.Component {
-    constructor({data}) {
-        super({data});
-        this.data = data;
-        this.state = {
-          searchTerm: '',
-        };
-        this.state.searchResult = [];
-        if (this.data.allJavascriptFrontmatter)
-          this.state.allItems = this.data.allJavascriptFrontmatter.edges;
-        else this.state.allItems = [];
-        this.onChange = this.onChange.bind(this);
-    } 
-    onChange(e) {
-        const value = e.target.value;
-        let searchRes = [];
-        this.setState( { searchTerm: value});
-        if (value !== '') {
-          searchRes = this.state.allItems.filter(item => {
-            return item.node.frontmatter.name
-                .concat(item.node.frontmatter.place_of_birth)
-                .toLowerCase()
-                .includes(value.toLowerCase());
-          });
-        }
-        this.setState({ searchResult: searchRes });
-        console.log('val ' + value);
-        
+  constructor({ data }) {
+    super({ data })
+    this.data = data
+    this.state = {
+      searchTerm: "",
     }
-    render() {
-      return (
+    this.state.searchResult = []
+    if (this.data.allJavascriptFrontmatter)
+      this.state.allItems = this.data.allJavascriptFrontmatter.edges
+    else this.state.allItems = []
+    this.onChange = this.onChange.bind(this)
+  }
+  onChange(e) {
+    const value = e.target.value
+    let searchRes = []
+    this.setState({ searchTerm: value })
+    if (value !== "") {
+      searchRes = this.state.allItems.filter(item => {
+        return item.node.frontmatter.name
+          .concat(item.node.frontmatter.place_of_birth)
+          .toLowerCase()
+          .includes(value.toLowerCase())
+      })
+    }
+    this.setState({ searchResult: searchRes })
+    console.log("val " + value)
+  }
+  render() {
+    return (
+      <Layout>
         <div className="container">
-          <h1>Белорусские режиссёры театра</h1>
           <form action="">
             <input
               type="search"
@@ -44,16 +44,22 @@ class Search extends React.Component {
             <div class="fa fa-search"></div>
           </form>
           <div className="search-result">
-              { 
-                this.state.searchResult.map(item => <a key={item.node.frontmatter.id} href={item.node.frontmatter.path}>{item.node.frontmatter.name}</a>)
-              }
+            {this.state.searchResult.map(item => (
+              <a
+                key={item.node.frontmatter.id}
+                href={item.node.frontmatter.path}
+              >
+                {item.node.frontmatter.name}
+              </a>
+            ))}
           </div>
         </div>
-      )
-    }
+      </Layout>
+    )
+  }
 }
 
-export default Search;
+export default Search
 
 export const queryResult = graphql`
   query MyQuery {
