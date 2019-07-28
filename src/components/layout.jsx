311b59e4f8bb3a521564da2ba827f11ac18f5311
Vola Graholska <vola.graholska@gmail.com>
2019-07-28 19:44:37 +0300

    feat: add layout style

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 import Footer from './footer';
 import Header from './header';
-import '../styles/style.css'
+import layoutStyles from './styles/layout.module.css'
+import '../styles/style.css';
 
 const Layout = (props) => {
     return (
-        <div>
-            <Header />
-            {props.children}
+        <div className={layoutStyles.wrapper}>
+            <div className={layoutStyles.content}>
+                <Header />
+                {props.children}
+            </div>
             <Footer />
         </div>
     )
