419e26fae0acc1e454d75c0aee8f6ca765114b16
Vola Graholska <vola.graholska@gmail.com>
2019-08-01 00:58:38 +0300

    feat:  add title for author page

---
+++ src/components/componentsForAuthorPage/title.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Row, Col} from 'react-bootstrap';
+import TitleStyle from './authorStyles/title.module.css'
+
+
+const Title = (authorInfo) => {
+    return (
+        <div className={TitleStyle.author_header_container}>
+            <Container>
+            <Row>
+                <Col sm={4}>
+                    <img 
+                        src={'../../images/'+ authorInfo.img}
+                        alt={authorInfo.name}
+                    />
+                </Col>
+                <Col sm={8}>
+                    <div className={TitleStyle.author_info}>
+                        <h2 className={TitleStyle.author_title}>{authorInfo.name}</h2>
+                        <p className={TitleStyle.birth_date}>{authorInfo.date}</p>
+                        <p className={TitleStyle.author_description}>{authorInfo.description}</p>
+                    </div>
+                </Col>
+            </Row>
+            </Container>
+
+        </div>
+    );
+};
+
+export default Title;
