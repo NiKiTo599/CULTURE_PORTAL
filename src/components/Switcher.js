f2cc0cc65160bd58689037678378737d93802e0b
Cat <jarolika30@gmail.com>
2019-08-03 16:52:23 +0300

    feat: make translation of the footer, header, layout components

--- src/components/Switcher.js
+++ src/components/Switcher.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import { Language } from 'gatsby-plugin-i18next';
-
 import headerStyle from './styles/header.module.css';
 
 const Switcher = ({ changeLng, lng, availableLngs }) => (
-    <select  className={ headerStyle.lang_select } onChange={e => changeLng(e.target.value)}>
-      {availableLngs.map(value => (
-        <option key={value} selected={lng === value}>
-          {value}
-        </option>
-      ))}
-      ;
-    </select>
-  );
-  
-  export default props => (
-    <Language>{lngProps => <Switcher {...props} {...lngProps} />}</Language>
-  );
-  
+  <select  className={ headerStyle.lang_select } onChange={e => changeLng(e.target.value)}>
+    {availableLngs.map(value => (
+      <option key={value} selected={lng === value}>
+        {value}
+      </option>
+    ))}
+    ;
+  </select>
+);
+
+export default props => (
+  <Language>{lngProps => <Switcher {...props} {...lngProps} />}</Language>
+);
