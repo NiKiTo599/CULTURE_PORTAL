f44fbdcd791e1a62b5a4c486153de08e7a810955
NiKiTo599 <akulichnikita0@gmail.com>
2019-08-03 15:28:18 +0300

    fix: change graphql request in all files

--- src/components/authorPage.js
+++ src/components/authorPage.js
@@ -1,51 +1,56 @@
 import React from "react"
-import Layout from "../components/layout";
-import Title from './componentsForAuthorPage/title'
-import Timeline from './componentsForAuthorPage/timeline'
-import Gallery from './componentsForAuthorPage/gallery'
-import AuthorWork from './componentsForAuthorPage/authorWork'
-import MapComponent from './componentsForAuthorPage/MapComponent';
+import Layout from "../components/layout"
+import Title from "./componentsForAuthorPage/title"
+import Timeline from "./componentsForAuthorPage/timeline"
+import Gallery from "./componentsForAuthorPage/gallery"
+import AuthorWork from "./componentsForAuthorPage/authorWork"
+import MapComponent from "./componentsForAuthorPage/MapComponent"
 
 const athorPage = ({ data }) => {
-  const infoOfAuthor = data.javascriptFrontmatter.frontmatter;
+  const infoOfAuthor = data.allFile.edges[0].node;
   return (
-      <Layout>
-        <Title { ...infoOfAuthor }/>
-        <Timeline { ...infoOfAuthor } />
-        <AuthorWork { ...infoOfAuthor } />
-        <Gallery { ...infoOfAuthor } />
-        <MapComponent places={infoOfAuthor.places_for_map}/>
-      </Layout>
+    <Layout>
+      <Title {...infoOfAuthor} />
+      <Timeline {...infoOfAuthor} />
+      <AuthorWork {...infoOfAuthor} />
+      <Gallery {...infoOfAuthor} />
+      <MapComponent places={infoOfAuthor.places_for_map} />
+    </Layout>
   )
 }
 export default athorPage
+
 export const postQuery = graphql`
   query($path: String!) {
-    javascriptFrontmatter(frontmatter: { path: { eq: $path } }) {
-      frontmatter {
-        path
-        name
-        date
-        img
-        video_id
-        places_for_map {
-          name
-          width
-          length
-        }
-        place_of_birth
-        description
-        timeline {
-          date
-          text
-        }
-        works {
-          subject
-          names
-        }
-        gallery {
-          img
-          description
+    allFile(filter: { childDataJson: { path: { eq: $path } } }) {
+      edges {
+        node {
+          childDataJson {
+            path
+            name
+            date
+            img
+            video_id
+            places_for_map {
+              name
+              width
+              length
+            }
+            place_of_birth
+            description
+            timeline {
+              date
+              text
+            }
+            works {
+              subject
+              names
+            }
+            gallery {
+              img
+              description
+            }
+          }
         }
       }
     }
